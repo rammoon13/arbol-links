edd9f4be4c6d09b51bc5240416db42e5dc2b3fab
rammoon13 <herreraroblesramon@gmail.com>
2025-06-27 11:51:53 +0200

    mejor titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="header">
       {<img src={avatar} alt="Foto de perfil" className="avatar" /> }
       <h1>Ramón Herrera Robles</h1>
-      <p className="subtitle">Desarrollador de Aplicaciones Multiplataforma</p>
+      <p className="subtitle">Desarrollador de Software Full Stack · Creativo · Resolutivo</p>
     </header>
   )
 }
